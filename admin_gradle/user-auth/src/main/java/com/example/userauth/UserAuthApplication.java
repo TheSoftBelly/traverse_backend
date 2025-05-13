91fbec6540a6b386dee9fc38f46eb5f63fa0b858
TheSoftBelly <jjoon102426@gmail.com>
2025-05-14 00:23:30 +0900

    서버안정화 후 최신화

--- admin_gradle/user-auth/src/main/java/com/example/userauth/UserAuthApplication.java
+++ admin_gradle/user-auth/src/main/java/com/example/userauth/UserAuthApplication.java
@@ -2,11 +2,13 @@ package com.example.userauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 // Ensure that the required Spring Cloud dependency is present in the project's build configuration (e.g., Maven or Gradle).
 
 @SpringBootApplication
 @EnableScheduling  // 스케줄링 기능 활성화
+@EnableDiscoveryClient // Eureka Client 활성화
 public class UserAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserAuthApplication.class, args);
