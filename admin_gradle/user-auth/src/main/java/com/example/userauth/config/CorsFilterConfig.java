dbe503c4053dd7a68879d6b862690f05bcccd038
HyoJoon <jjoon102426@gmail.com>
2025-05-22 12:54:20 +0900

    도메인 변경

--- admin_gradle/user-auth/src/main/java/com/example/userauth/config/CorsFilterConfig.java
+++ admin_gradle/user-auth/src/main/java/com/example/userauth/config/CorsFilterConfig.java
@@ -19,7 +19,7 @@ public class CorsFilterConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://54.173.242.110:3000"));
+        config.setAllowedOrigins(Collections.singletonList("http://211.187.162.65:3000/"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
 
