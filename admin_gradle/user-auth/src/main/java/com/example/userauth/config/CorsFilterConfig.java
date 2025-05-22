6772f18497d94b1284fa6e5aa5f0fea643f7d893
HyoJoon <jjoon102426@gmail.com>
2025-05-22 14:50:59 +0900

    수정

--- admin_gradle/user-auth/src/main/java/com/example/userauth/config/CorsFilterConfig.java
+++ admin_gradle/user-auth/src/main/java/com/example/userauth/config/CorsFilterConfig.java
@@ -7,8 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
 public class CorsFilterConfig {
@@ -19,9 +18,11 @@ public class CorsFilterConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://211.187.162.65:3000", "http://localhost:3000"));
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedOrigins(Arrays.asList("http://***:3000", "http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+        config.setMaxAge(3600L);
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
