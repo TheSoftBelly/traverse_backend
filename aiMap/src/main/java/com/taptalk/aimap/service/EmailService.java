8ddfece136905446bde06b5ba5084dce4be87035
HyoJoon <jjoon102426@gmail.com>
2025-05-25 19:52:03 +0900

    aimap 수정

--- aiMap/src/main/java/com/taptalk/aimap/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.taptalk.aimap.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmailService {
-    void sendVerificationEmail(String email, String code);
-    boolean verifyCode(String email, String code);
-} 
