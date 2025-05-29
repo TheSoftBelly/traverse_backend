7c806f5efdff540fa8e7423e2df5661630bb3d5d
HyoJoon <jjoon102426@gmail.com>
2025-05-29 18:05:51 +0900

    .

---
+++ init-scripts/01-init-databases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Create userdb if it doesn't exist
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS userdb;
+    GRANT ALL PRIVILEGES ON userdb.* TO 'root'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Create map_platform if it doesn't exist
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS map_platform;
+    GRANT ALL PRIVILEGES ON map_platform.* TO 'map_user'@'%';
+    FLUSH PRIVILEGES;
+EOSQL 
