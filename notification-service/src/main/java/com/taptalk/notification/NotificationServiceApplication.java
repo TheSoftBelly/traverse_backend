101a11c5d133843e5d0e46772b8b9f1a6ea4d249
TheSoftBelly <jjoon102426@gmail.com>
2025-06-02 00:57:42 +0900

    20250602-모든 세팅 최신화 (성공)

--- notification-service/src/main/java/com/taptalk/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/taptalk/notification/NotificationServiceApplication.java
@@ -1,15 +1,8 @@
 package com.taptalk.notification;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,19 +10,6 @@ public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
-
-	@Bean
-	public FirebaseApp firebaseApp() throws IOException {
-		GoogleCredentials credentials = GoogleCredentials.fromStream(
-			new ClassPathResource("firebase-credentials.json").getInputStream()
-		);
-		
-		FirebaseOptions options = FirebaseOptions.builder()
-			.setCredentials(credentials)
-			.build();
-			
-		return FirebaseApp.initializeApp(options);
 	}
 }
  
